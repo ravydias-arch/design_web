42c2b8c157773540b90cee01c53ee8749de648c6
ravydias-arch <ravy.dias@escolar.ifrn.edu.br>
2026-08-21 13:43:42 -0300

    criacao de codigo js  mais complexo

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,14 @@
 alert('Olá, Mundo!')
+
+var nasc = 2010;
+let nome = "Eric Ravy";
+const viva = true
+let altura = 1.74;
+
+if (viva){
+    let saudacao = "Olá "+ nome +"!";
+    let msg = `Altura ${altura}m | Idade ${2026-nasc}`;
+    alert(saudacao+"\n"+msg);
+}else{
+    alert("R.I.P");
+}
