628cfa1ee821d7d673309df50759b816ea47deb0
ravydias-arch <ravy.dias@escolar.ifrn.edu.br>
2026-08-21 14:14:46 -0300

    criacao calcidade

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,17 @@
-alert('Olá, Mundo!')
+var nasc = 2009;
+let nome = "Pedrina"
+const viva = true;
 
-var nasc = 2010;
-let nome = "Eric Ravy";
-const viva = true
-let altura = 1.74;
-
-if (viva){
-    let saudacao = "Olá "+ nome +"!";
-    let msg = `Altura ${altura}m | Idade ${2026-nasc}`;
-    alert(saudacao+"\n"+msg);
-}else{
-    alert("R.I.P");
+function calcIdade(ano=2026){
+    let idade = ano - nasc;
+    alert(`Dentro de funcao - Idade ${idade}`);
+    return idade;
 }
+
+calcIdade();
+/*
+alert(`Fora da funcao : Idade ${idade}`)
+Erro pois a variavel let nao existe fora do escopo dela
+*/  
+/* Forma orreta */
+alert(`Fora da funcao: chamando calcIdade ${calcIdade()}`);
