1c41f656509c5cda03eb3c9bf16a1b3325ae6217
ravydias-arch <ravy.dias@escolar.ifrn.edu.br>
2026-09-04 13:20:46 -0300

    adicionei o break na app3

--- scripts/app3.js
+++ scripts/app3.js
@@ -4,12 +4,12 @@ let dia = prompt("Escolha um dia da semana\nSendo 1:Domingo - 7:Sábado.");
 dia = Number(dia);
 
 switch(dia){
-    case 1: alert("Você escolheu domingo");
-    case 2: alert("Você escolheu segunda");
-    case 3: alert("Você escolheu terça");
-    case 4: alert("Você escolheu quarta");
-    case 5: alert("Você escolheu quinta");
-    case 6: alert("Você escolheu sexta");
-    case 7: alert("Você escolheu sábado");
-    default: alert("Dia inválido");
+    case 1: alert("Você escolheu domingo"); break;
+    case 2: alert("Você escolheu segunda"); break;
+    case 3: alert("Você escolheu terça"); break;
+    case 4: alert("Você escolheu quarta"); break;
+    case 5: alert("Você escolheu quinta"); break;
+    case 6: alert("Você escolheu sexta"); break;
+    case 7: alert("Você escolheu sábado"); break;
+    default: alert("Dia inválido"); break;
 }
