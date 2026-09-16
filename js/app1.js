a68fa60c0a5a21d52c8f10070ad091573c329c03
ravydias-arch <ravy.dias@escolar.ifrn.edu.br>
2026-09-16 17:54:55 -0300

    adicionei coisas em att8

--- js/app1.js
+++ js/app1.js
@@ -12,6 +12,7 @@ function proximo(e){
 const prox = document.getElementById("prox");
 prox.addEventListener("click", proximo);
 
+
 function anterior(e){
     e.preventDefault();
     if(i>1){
@@ -19,6 +20,57 @@ function anterior(e){
     }
     imagem.setAttribute("src",`./img/foto${i}.jpg`);
 }
-
 const ant = document.getElementById("ant");
 ant.addEventListener("click", anterior);
+function selecionar(e){
+    e.preventDefault();
+    i = Number(cbbox.value);
+    imagem.setAttribute("src",'./img/foto${i}.jpg');
+}
+const cbbox = document.getElementById("combobox");
+cbbox.addEventListener("change", selecionar);
+
+const opacidade = document.getElementById("opacidade");
+opacidade.addEventListener("input", function(e){
+    e.preventDefault();
+    let valor = opacidade.value;
+    foto.setAttribute("style", `opacity:${valor}`);
+});
+
+const li1 = document.getElementById("li1")
+li1.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 1;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`)
+})
+
+const li2 = document.getElementById("li2");
+l12.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 2;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`);
+})
+const li3 = document.getElementById("li3");
+l13.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 3;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`);
+});
+const li4 = document.getElementById("li4");
+l14.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 4;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`);
+});
+const li5 = document.getElementById("li5");
+l15.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 5;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`);
+});
+const li6 = document.getElementById("li6");
+l16.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    i = 6;
+    imagem.setAttribute("src", `./img/foto${i}.jpg`);
+});
